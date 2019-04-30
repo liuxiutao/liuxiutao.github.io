e2ed1ac32f3479de7c6dae9cd71d240d5c9f1e94
liutao93 <liutao93@hotmail.com>
2019-04-30 10:19:27 +0800

    Site updated: 2019-04-30 10:19:26

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [];
+var precacheConfig = [["/sw-register.js","bb512c4eae531758667789162b261e7d"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
